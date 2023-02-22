8fc2e0236837ba618f3a20dd82b5eab99adffc77
Akvirac <juanarielvivian@gmail.com>
2023-02-21 22:50:39 -0300

    avances3

--- pages/entradas/index.vue
+++ pages/entradas/index.vue
@@ -3,9 +3,13 @@
 var hNum = 0
 var mNum = 0
 var bNum = 0
+var vTicket = 0
 
 const totalNoche = ref([])
 //Funciones
+
+//Bandera para Numero de tickets.
+console.log(vTicket)
 //Contador hombres
 function ticketHombre() {
 	hNum = hNum + 1
@@ -27,6 +31,9 @@ function cerrarCaja() {
 	totalNoche.mujeres = mNum
 	totalNoche.binario = bNum
 	totalNoche.total = hNum + mNum + bNum
+	hNum = 0
+	mNum = 0
+	bNum = 0
 	console.log(totalNoche.hombres)
 	console.log(totalNoche.mujeres)
 	console.log(totalNoche.binario)
@@ -38,14 +45,14 @@ function cerrarCaja() {
 	<div>
 		<div class="flex justify-center pt-16">
 			<div>
-				<Boton class="bg-blue-700 hover:bg-blue-900" @click="ticketHombre"
+				<Boton class="bg-blue-700 hover:bg-blue-900" @click="valorTicket"
 					>Hombre</Boton
 				>
 				<div class="flex justify-center pt-2">
-					<BotonS class="hover:bg-blue-900">2</BotonS>
-					<BotonS class="hover:bg-blue-900">3</BotonS>
-					<BotonS class="hover:bg-blue-900">4</BotonS>
-					<BotonS class="hover:bg-blue-900">5</BotonS>
+					<BotonS @click="vTicket = 2" class="hover:bg-blue-900">2</BotonS>
+					<BotonS @click="valorTicket3" class="hover:bg-blue-900">3</BotonS>
+					<BotonS @click="valorTicket4" class="hover:bg-blue-900">4</BotonS>
+					<BotonS @click="valorTicket5" class="hover:bg-blue-900">5</BotonS>
 				</div>
 			</div>
 			<div>
